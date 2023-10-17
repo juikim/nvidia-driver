f80ee8decf905dc7c0fc959485c5b511a6d029b2
Jung-uk Kim <jkim@FreeBSD.org>
2023-10-17 13:06:27 -0400

    Remove linuxkpi_gplv2.ko dependency to sync. with drm-515-kmod 5.15.118

--- nvidia/src/nvidia-drm/nvidia-drm-freebsd-lkpi.c
+++ nvidia/src/nvidia-drm/nvidia-drm-freebsd-lkpi.c
@@ -228,7 +228,6 @@ int nv_drm_probe_devices(void)
 
 LKPI_DRIVER_MODULE(nvidia_drm, nv_drm_init, nv_drm_exit);
 MODULE_DEPEND(nvidia_drm, linuxkpi, 1, 1, 1);
-MODULE_DEPEND(nvidia_drm, linuxkpi_gplv2, 1, 1, 1);
 MODULE_DEPEND(nvidia_drm, drmn, 2, 2, 2);
 MODULE_DEPEND(nvidia_drm, dmabuf, 1, 1, 1);
 MODULE_DEPEND(nvidia_drm, nvidia, 1, 1, 1);
